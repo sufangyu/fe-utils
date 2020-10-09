4410176bfdbb1cc4023648cd5cc8c5fda4694f3a
sufangyu <sufangru@baibu.la>
2020-10-10 00:28:55 +0800

    feat: 增加 数组分割工具函数

---
+++ tests/array/index.test.ts
@@ -0,0 +1,38 @@
+import { sliceArray,sliceArrayCompleteLength } from '../../src/array';
+
+
+describe('数组:', () => {
+  const arr = [1, 2, 3, 4, 5, 6];
+
+  it('不够完整分割', () => {
+    const result = sliceArray(arr, 4);
+    expect(result).toEqual([[1, 2, 3, 4], [5, 6]]);
+  });
+
+  it('够完整分割', () => {
+    const result = sliceArray(arr, 3);
+    expect(result).toEqual([[1, 2, 3], [4, 5, 6]]);
+  });
+
+  it('空数组分割', () => {
+    const result = sliceArray([], 4);
+    expect(result).toEqual([]);
+  });
+
+
+  it('补全长度: 不够完整分割', () => {
+    const result = sliceArrayCompleteLength(arr, 3);
+    expect(result).toEqual([[1, 2, 3], [4, 5, 6]]);
+  });
+
+  it('补全长度: 够完整分割', () => {
+    const result = sliceArrayCompleteLength(arr, 4);
+    expect(result).toEqual([[1, 2, 3, 4], [5, 6, '', '']]);
+  });
+
+  it('补全长度: 空数组', () => {
+    const result = sliceArrayCompleteLength([], 4);
+    expect(result).toEqual([]);
+  });
+});
+
